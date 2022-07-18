79221cc185361635826c43b93c5ca5d50484143b
enzojb <102626586+enzojb@users.noreply.github.com>
2022-07-18 20:51:29 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = express();
-const port = 3030;
+const port = process.env.PORT || 3030;
 const start = () => console.log("Starting server...");
 const path = require("path");
 const public = path.join(__dirname, "../public");
